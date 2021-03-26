0b487d0d09d6896b8a86daae043e7279b339d906
Kennn-dev <nhatong2015@gmail.com>
2021-03-26 19:32:27 +0700

    auth middleware added

---
+++ middlewares/auth.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const jwtAuth = async (req, res, next) => {
+  y;
+  const tokenFromRequest = await req.headers.authorization;
+  // console.log({ tokenFromRequest });
+
+  if (tokenFromRequest) {
+    //verify token
+    try {
+      const token = tokenFromRequest.replace("Bearer ", "");
+      const verifiedToken = jwt.verify(token, process.env.ACCESS_TOKEN);
+
+      //save info token then next()
+      req.user = verifiedToken;
+      next();
+    } catch (error) {
+      //not valid token
+
+      res.status(401);
+      res.send({ error: "Unauthorized 🔐" });
+    }
+  } else {
+    //not have token
+    res.status(403).send({ error: "Who are you ??? 🤔" });
+  }
+};
+
+const getNewAccessToken = async (refreshToken) => {
+  try {
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN);
+
+    const newAccessToken = jwt.sign(decoded, process.env.ACCESS_TOKEN, {
+      expiresIn: "1h",
+    });
+    return newAccessToken;
+  } catch (error) {
+    return null;
+  }
+};
+
+module.exports = {
+  jwtAuth: jwtAuth,
+  getNewAccessToken: getNewAccessToken,
+};
